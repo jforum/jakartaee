bbb84dc256b92134dbfb50e0721320023a55cfd2
Ulf Dittmer <ulf.dittmer@googlemail.com>
2020-08-07 08:42:57 +0000

    comment about syntax-highlighting the preview

--- src/main/resources/templates/default/ajax_preview_post.js
+++ src/main/resources/templates/default/ajax_preview_post.js
@@ -2,6 +2,10 @@ $("#previewSubject").html("${post.subject?html}");
 $("#previewMessage").html("${post.text}");
 $("#previewTable").show();
 
+//Prism.highlightAll();
+// This inlines everything: Using the Keep Markup plugin according to
+// https://github.com/PrismJS/prism/issues/832 did not solve the issue, as now the linenumbers are out of whack.
+
 var s = document.location.toString();
 var index = s.indexOf("#preview");
 
