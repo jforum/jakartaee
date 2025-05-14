847a0f4953ccf31391978073b88b2906320a740e
Ulf Dittmer <ulf.dittmer@googlemail.com>
2025-05-14 08:22:20 +0000

    update dependencies

--- src/main/java/net/jforum/JForumBaseServlet.java
+++ src/main/java/net/jforum/JForumBaseServlet.java
@@ -86,7 +86,7 @@ public class JForumBaseServlet extends HttpServlet
 			ConfigLoader.startCacheEngine();
 
 			// Configure the template engine
-			final Configuration templateCfg = new Configuration(Configuration.VERSION_2_3_33);
+			final Configuration templateCfg = new Configuration(Configuration.VERSION_2_3_34);
 			if (isDevelopment) {
 				templateCfg.setTemplateUpdateDelayMilliseconds(2000);
 			} else {
